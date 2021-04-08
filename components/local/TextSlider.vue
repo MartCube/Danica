6168645d1c8ff9370183c91dead6b630f581015a
Marya <marya.vadymovna@gmail.com>
2021-04-08 18:37:35 +0300

    TextSlider animation

--- components/local/TextSlider.vue
+++ components/local/TextSlider.vue
@@ -1,12 +1,15 @@
 <template>
-	<n-link class="text_slider" :to="link">
-		<template v-if="active">
+	<div class="text_slider" >
+		<div class="first">
 			<span v-for="(letter, i) in lettersDesign" :key="i">{{ letter }}</span>
-		</template>
-		<template v-else>
+		</div>
+		<div class="second">
 			<span v-for="(letter, i) in lettersArchitecture" :key="i">{{ letter }}</span>
-		</template>
-	</n-link>
+		</div>
+		<!-- <template v-else> -->
+			<!-- <span v-for="(letter, i) in lettersArchitecture" :key="i">{{ letter }}</span> -->
+		<!-- </template> -->
+	</div>
 </template>
 
 <script>
@@ -17,6 +20,7 @@ export default {
 	data: () => ({
 		active: true,
 		link: '/services/design',
+		letters : null
 	}),
 	computed: {
 		lettersArchitecture() {
@@ -27,50 +31,74 @@ export default {
 		},
 	},
 	mounted() {
-		setTimeout(() => this.Animate(), 2000)
+		this.Animate()
+		this.letters = this.lettersDesign
+		// setTimeout(() => this.letters = , 2000)
 	},
 	methods: {
-		async Animate() {
-			await this.$nextTick()
+		 Animate() {
+			let loops = 0;
+			let el = document.querySelectorAll('.text_slider span');
+			// await this.$nextTick()
 
 			const StartUpTimeline = anime.timeline({
-				autoplay: true,
-			})
-			const el = document.querySelectorAll('.text_slider span')
-			const duration = 1000
-
-			StartUpTimeline.add({
-				targets: el,
-				opacity: [0, 1],
-				rotateY: [90, 0],
-
-				delay: anime.stagger(50, { from: 'first' }),
+				// autoplay: true,
 				easing: 'easeOutExpo',
-				duration,
-			})
-				.add({
-					targets: el,
-					duration: 3000,
-				})
-				.add({
-					targets: el,
-					opacity: [1, 0],
-					rotateY: [0, -90],
+				loop: true,
+				direction: 'alternate',
+				duration: 2000,
+				changeComplete: (e) => {
+					// this.letters = this.lettersArchitecture
+				},
+				update: (e) => {
+					// logEl.innerHTML = JSON.stringify(battery);
+					++loops
+					console.log(e, loops);
 
-					delay: anime.stagger(50, { from: 'last' }),
-					easing: 'easeOutExpo',
-					duration,
-				})
+				},
+				// loopComplete: (e) => {
+				// 	++loops
+				// 	console.log(e, loops);
+				// 	if(loops % 2 === 0 ){
+				// 		this.letters = this.lettersArchitecture;
+				// 		el = document.querySelectorAll('.text_slider span')
+				// 	}
+				// 	else {
+				// 		this.letters = this.lettersDesign;
+				// 		el = document.querySelectorAll('.text_slider span')
+				// 	}
+				// },
+			}).add({
+				targets: ".first span",
+				opacity: [0, 1],
+				direction: "forwards",
+				delay: anime.stagger(100),
+			},
+			2000
+			).add({
+				targets: ".second span",
+				opacity: [0, 1],
+				direction: "forwards",
+				delay: anime.stagger(100),
+			},
+			2000)
+
 
 			StartUpTimeline.finished.then(() => {
-				// changing the array of letters
-				this.active = !this.active
-				// changing the link
-				if (this.active) this.link = '/services/design'
-				else this.link = '/services/architecture'
-				this.Animate()
+			// 	// changing the array of letters
+			// 	this.active = !this.active
+			// // 	// changing the link
+			// 	if (this.active) this.link = '/services/design'
+			// 	else this.link = '/services/architecture'
+			// 	// this.Animate()
 			})
 		},
+		// loop(t) {
+		// 	StartUpTimeline.tick(t);
+		// 	customRAF = requestAnimationFrame(loop);
+			
+		// },
+		
 	},
 }
 </script>
@@ -83,16 +111,25 @@ export default {
 	overflow: hidden;
 
 	width: max-content;
-	display: flex;
-	align-items: center;
-	z-index: 2;
+	.first, .second{
+		position: absolute;
+		display: flex;
+		align-items: center;
+		z-index: 2;
+		min-height: 6rem;
+	}
 	span {
 		opacity: 0; // opacity: 1
-
+		transform: translateY(-10px);
 		text-transform: none;
 		line-height: initial;
 		font-weight: 700;
 		font-size: 4.5vw;
+		min-width: 1rem;
+		display: flex;
+		will-change: opacity, transform;
+		// transition: opacity, transform 1s ease-out;
+		// animation: titleLetters 1s linear infinite alternate;
 	}
 
 	&::after {
@@ -111,6 +148,14 @@ export default {
 		}
 	}
 }
+
+
+
+@keyframes titleLetters {
+	
+}
+
+
 @media (max-width: 900px) {
 	.text_slider span{
 		font-size: 8.5vw;

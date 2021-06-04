318dcf56781aeee839dad5d22645b49abd7478ca
mart cube <martingjorceski@gmail.com>
2021-06-04 16:40:47 +0200

    onlyOnRoot to false

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,7 +78,7 @@ export default {
 		detectBrowserLanguage: {
 			useCookie: true,
 			cookieKey: 'i18n_redirected',
-			onlyOnRoot: true, // recommended
+			onlyOnRoot: false,
 		},
 		lazy: true,
 		seo: false,

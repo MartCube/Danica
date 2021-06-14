e3298541fff0f9603efee3378b19cb7b3cb826a9
mart cube <martingjorceski@gmail.com>
2021-06-14 19:09:35 +0200

    fb chat

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,6 +17,26 @@ export default {
 		await this.$nextTick()
 		this.$fb.enable()
 		this.$gtm.init('GTM-T5X9PFF')
+		;(function (d, s, id) {
+			let js
+			const fjs = d.getElementsByTagName(s)[0]
+			if (d.getElementById(id)) {
+				return
+			}
+			js = d.createElement(s)
+			js.id = id
+			js.src = 'https://connect.facebook.net/en_US/sdk.js'
+			fjs.parentNode.insertBefore(js, fjs)
+		})(document, 'script', 'facebook-jssdk')
+		window.fbAsyncInit = function () {
+			window.FB.init({
+				appId: '1303536193006608',
+				autoLogAppEvents: true,
+				xfbml: true,
+				version: 'v2.10',
+			})
+			window.FB.AppEvents.logPageView()
+		}
 	},
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -113,18 +113,18 @@ export const actions = {
 
 			// links & meta
 			head.link.push({ hid: 'alternate', rel: 'alternate', href, hreflang: altLang })
-			head.meta.push({ hid: 'og:url', name: 'og:locale:alternate', content: href })
+			// head.meta.push({ hid: 'og:url', name: 'og:locale:alternate', content: href })
 		})
 
 		head.meta.push(
 			...[
 				{ hid: 'description', name: 'description', content: fetch.data.meta_description },
 				// facebook
-				{ hid: 'og:type', name: 'og:type', content: '' },
-				{ hid: 'og:url', name: 'og:url', content: canonical },
-				{ hid: 'og:title', name: 'og:title', content: fetch.data.meta_title },
-				{ hid: 'og:description', name: 'og:description', content: fetch.data.meta_description },
-				{ hid: 'og:image', name: 'og:image', content: fetch.data.meta_image === undefined ? '' : fetch.data.meta_image.url },
+				{ hid: 'og:type', property: 'og:type', content: '' },
+				{ hid: 'og:url', property: 'og:url', content: canonical },
+				{ hid: 'og:title', property: 'og:title', content: fetch.data.meta_title },
+				{ hid: 'og:description', property: 'og:description', content: fetch.data.meta_description },
+				{ hid: 'og:image', property: 'og:image', content: fetch.data.meta_image === undefined ? '' : fetch.data.meta_image.url },
 				// twitter
 				{ hid: 'twitter:card', name: 'twitter:card', content: fetch.data.meta_image === undefined ? '' : fetch.data.meta_image.url },
 			],

088caf301fb8abd454f1ee98fe1b41f1a78e495a
Marya <marya.vadymovna@gmail.com>
2021-07-06 19:12:38 +0200

    ...

--- plugins/fb-chat.js
+++ plugins/fb-chat.js
@@ -1,16 +1,8 @@
+import Vue from 'vue'
+import VueFbCustomerChat from 'vue-fb-customer-chat'
 
-
-setTimeout(() => {
-	// import VueFbCustomerChat from 'vue-fb-customer-chat'
-	// import Vue from 'vue'
-
-		const Vue = () => import('vue')
-	const VueFbCustomerChat = () => import('vue-fb-customer-chat')
-	// import VueFbCustomerChat from 'vue-fb-customer-chat'
-	console.log(VueFbCustomerChat, Vue);
-	Vue.use(VueFbCustomerChat, {
-		page_id: 1303536193006608, //  change 'null' to your Facebook Page ID,
-		theme_color: '#333333', // theme color in HEX
-		// locale: 'en_US', // default 'en_US'
-	})
-}, 3500);
+Vue.use(VueFbCustomerChat, {
+	page_id: 1303536193006608, //  change 'null' to your Facebook Page ID,
+	theme_color: '#333333', // theme color in HEX
+	locale: 'en_US', // default 'en_US'
+})

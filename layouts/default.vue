bc70766e68203e20ae265c2f0bd901c380ef2cef
Marya <marya.vadymovna@gmail.com>
2021-07-13 11:59:25 +0200

    fb chat removed , bitrix chat added

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,13 +17,12 @@ export default {
 	components: { CookieBox },
 	data: () => ({}),
 	async mounted() {
-		
 		await this.$nextTick()
 		setTimeout(() =>{
 			this.$fb.enable()
 			this.$gtm.init('GTM-T5X9PFF')
+			(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://crm.danica.ua/upload/crm/site_button/loader_1_omb21n.js');
 		}, 5500)
-	
 	},
 }
 </script>

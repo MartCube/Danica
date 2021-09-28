d38a016110f45212f031687bddb60dbbea4319d7
Marya <marya.vadymovna@gmail.com>
2021-09-28 20:23:27 +0200

    link resolver about us

--- plugins/link-resolver.js
+++ plugins/link-resolver.js
@@ -2,13 +2,13 @@ export default function (doc, parent) {
 	// Return the path depending on Prismic Document's type
 
 	// If it is a Repeatable Custom Type with the API ID
-	if (doc.type === 'services') {
+	if (doc.type === 'services' || doc.type === 'about_us') {
 		return '/' + doc.uid
 	} else if (doc.type === 'blog_post') {
 		return '/blog/' + doc.uid
 	} else if (doc.type === 'project_post') {
 		return '/projects/' + doc.uid
-	}
+	} 
 	// else if (doc.type === 'service_second') {
 	// 	// console.log(doc, parent);
 	// 	if (parent !== undefined) return `/${ parent.uid }/${ doc.uid }`
